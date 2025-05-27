6c50b36d26c585d72ae819fc04c05fd00deed324
Dimitri <dim.de.bleser@gmail.com>
2025-05-27 10:13:51 +0200

    add entity framework

--- obj/Debug/net9.0/SchedulingAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/SchedulingAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchedulingAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+611c119009f4e64ebc487d22d876d2502a0d02a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchedulingAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchedulingAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
